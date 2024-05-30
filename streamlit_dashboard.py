dac6241765a20d7caff3d6a3c4b2e6d8cae2ef92
Preston Khiev <44714937+pkhiev@users.noreply.github.com>
2024-05-30 23:30:40 +0000

    Testing packages

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
 
+
+st.set_page_config(
+    page_title="Product Data for Armstrong Ceilings",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+df = pd.read_csv('ArmstrongProductList.csv')
+
+print(df)
